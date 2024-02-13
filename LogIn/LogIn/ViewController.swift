349699c2b2fc7defe87739bfc8cd975a83a308ac
FazillD <m.fazildemircan@gmail.com>
2024-02-13 16:19:46 +0300

    Simple Log In page design example

---
+++ LogIn/LogIn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogIn
+//
+//  Created by Fazıl Demircan on 13.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
